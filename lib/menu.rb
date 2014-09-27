dcdd9cc8767c208ed01e216a3199cd61b62209cf
yvettecook <yvettecook@gmail.com>
2014-09-27 19:39:41 +0100

    Menu can now return a list of dishes and prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -10,6 +10,12 @@ class Menu
 		dish_list << [dish.name, dish.price]
 	end
 
+	def display_menu
+		@display_menu = dish_list.collect do | dish |
+			"#{dish[0]}: #{dish[1]}"
+		end
+	end
+
 
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,10 +4,18 @@ describe 'Menu' do
 
 	let (:menu) { Menu.new }
 	let (:dish1) { double :dish, :name => 'trifle', :price => 4 }
+	let (:dish2) { double :dish, :name => 'brownie', :price => 5 }
 	
 	it "should be a list of dishes and prices" do
 		menu.add(dish1)
 		expect(menu.dish_list).to include(['trifle', 4])
 	end
+
+	it "should return a list of dishes and prices" do
+		menu.add(dish1)
+		menu.add(dish2)
+		expect(menu.display_menu).to eq(["trifle: 4", "brownie: 5"])
+	end
+
 
 end
